6ddcc77a6395dabc26ba687a7e41bcce917dc403
ubimars <marcelpenton+github@gmail.com>
2021-05-08 19:49:21 -0400

    Update SAMPLE_DESCRIPTION

--- app_config.py
+++ app_config.py
@@ -5,7 +5,7 @@ SESSION_TYPE = 'filesystem'
 
 
 # sample title in navbar
-SAMPLE_DESCRIPTION = 'Authorization: Use MSAL Python to obtain an access token to call MS Graph'
+SAMPLE_DESCRIPTION = 'Authorization: Use MSAL Python'
 
 
 # MS Graph API endpoint
